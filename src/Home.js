048b2cf0e6e433916373fee97acbba241081bf25
Ankit Saikia <112230331+Saikiasan@users.noreply.github.com>
2023-01-27 20:34:46 +0530

    error

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import Navigator from './comp/Navbar'
 import Footer from './comp/Footer'
+import Typed from 'typed.js'
 
 const Home = () => {
   // hiding the icons number
@@ -8,8 +10,10 @@ const Home = () => {
   icons_d.forEach((icon) => {
     icon.classList.add('visually-hidden')
   })
+
   return (
-    <div  className='main mt-5'>
+    <div  className='main'>
+      <Navigator/>
       <div className='container-fluid'>
         <div className='row gy-4 row-cols-2 row-cols-md-4 text-light showcase-1'>
 
@@ -92,14 +96,14 @@ const Home = () => {
             <div className='text-center d-flex flex-column justify-content-center align-items-center py-3'>
               <div
               className='d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block mb-2 lg'>
-              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -64 640 640' height='1em' width='1em' fill='white' style={{fontSize:'80px'}}>
-                <path d='M414.8 40.79L286.8 488.8C281.9 505.8 264.2 515.6 247.2 510.8C230.2 505.9 220.4 488.2 225.2 471.2L353.2 23.21C358.1 6.216 375.8-3.624 392.8 1.232C409.8 6.087 419.6 23.8 414.8 40.79H414.8zM518.6 121.4L630.6 233.4C643.1 245.9 643.1 266.1 630.6 278.6L518.6 390.6C506.1 403.1 485.9 403.1 473.4 390.6C460.9 378.1 460.9 357.9 473.4 345.4L562.7 256L473.4 166.6C460.9 154.1 460.9 133.9 473.4 121.4C485.9 108.9 506.1 108.9 518.6 121.4V121.4zM166.6 166.6L77.25 256L166.6 345.4C179.1 357.9 179.1 378.1 166.6 390.6C154.1 403.1 133.9 403.1 121.4 390.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4L121.4 121.4C133.9 108.9 154.1 108.9 166.6 121.4C179.1 133.9 179.1 154.1 166.6 166.6V166.6z'/>
-              </svg>
-            </div>
-            <div className='px-3'>
-              <h2 className='fw-bold mb-0 ico_num'>10+</h2>
-              <p className='mb-0'>Code</p>
-            </div>
+                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -64 640 640' height='1em' width='1em' fill='white' style={{fontSize:'80px'}}>
+                  <path d='M414.8 40.79L286.8 488.8C281.9 505.8 264.2 515.6 247.2 510.8C230.2 505.9 220.4 488.2 225.2 471.2L353.2 23.21C358.1 6.216 375.8-3.624 392.8 1.232C409.8 6.087 419.6 23.8 414.8 40.79H414.8zM518.6 121.4L630.6 233.4C643.1 245.9 643.1 266.1 630.6 278.6L518.6 390.6C506.1 403.1 485.9 403.1 473.4 390.6C460.9 378.1 460.9 357.9 473.4 345.4L562.7 256L473.4 166.6C460.9 154.1 460.9 133.9 473.4 121.4C485.9 108.9 506.1 108.9 518.6 121.4V121.4zM166.6 166.6L77.25 256L166.6 345.4C179.1 357.9 179.1 378.1 166.6 390.6C154.1 403.1 133.9 403.1 121.4 390.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4L121.4 121.4C133.9 108.9 154.1 108.9 166.6 121.4C179.1 133.9 179.1 154.1 166.6 166.6V166.6z'/>
+                </svg>
+              </div>
+              <div className='px-3'>
+                <h2 className='fw-bold mb-0 ico_num'>10+</h2>
+                <p className='mb-0'>Code</p>
+              </div>
             </div>
           </div>
 
